4d1573429fdd4793326e0f8a820e6331cda54201
kasule joseph <joseph.kasule@andela.com>
2019-11-23 11:23:20 +0200

    add style and script files

--- pdf-viewer/script.js
+++ pdf-viewer/script.js
@@ -4,13 +4,13 @@ var app = new Vue({
     data: {
         file: null,
         preview: null,
-        yy: null,
+        imageArray: [],
         pages: []
     },
     methods: {
         selectImage(page) {
-            console.log('page', page, this.yy);
-            this.preview = `https://res.cloudinary.com/dcsrepenv/image/upload/w_350,h_400,c_fill,pg_${page}/${this.file.public_id}.jpg`
+            const public_id = this.imageArray.filter(element => typeof(element) === "string")
+            this.preview = `https://res.cloudinary.com/dcsrepenv/image/upload/q_100,dn_50,w_350,h_400,c_fill,pg_${page}/${public_id[0]}.jpg`
         },
         openWidget(url) {
             window.cloudinary.openUploadWidget(
@@ -29,15 +29,13 @@ var app = new Vue({
                 (error, result) => {
                     console.log(error, result);
                     this.file = result.info
-                    this.yy = result.info.public_id
-                    console.log('this.yy', this.yy);
-                    console.log('this.file', this.file);
-
-                    this.preview = `https://res.cloudinary.com/dcsrepenv/image/upload/w_350,h_400,c_fill,pg_1/${this.yy}.jpg`;
+                    this.imageArray.push(result.info.public_id)
+                    const public_id = this.imageArray.filter(element => typeof(element) === "string")
+                    this.preview = `https://res.cloudinary.com/dcsrepenv/image/upload/q_100,dn_50,w_350,h_400,c_fill,pg_1/${public_id[0]}.jpg`;
                     for(let i =1; i<=this.file.pages; i++) {
                         this.pages.push(
                             {
-                                url: `https://res.cloudinary.com/dcsrepenv/image/upload/w_200,h_250,c_fill,pg_${i}/${this.file.public_id}.jpg`,
+                                url: `https://res.cloudinary.com/dcsrepenv/image/upload/q_100,dn_50,w_200,h_250,c_fill,pg_${i}/${this.file.public_id}.jpg`,
                                 page: i
                             }
                         )
